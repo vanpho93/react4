6f48fefe640cde027a5a08e67762e1a8f0b5a855
Khoa Pham <khoaphp@yahoo.com>
2016-11-08 02:51:29 -0800

    bat dau

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static('public'));
+
+server.listen(3000);
+
+app.get('/', function(req, res){
+  res.render('trangchu');
+});
+
+io.on('connection', function(socket){
+  console.log('New user connected');
+});
